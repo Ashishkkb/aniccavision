75baaa91999b1292ceee61e27390a8aa64863373
suraj.panda <suraj.panda@aniccadata.in>
2024-02-21 21:25:42 +0530

    deployment changes

--- src/app/components/Dashboard/index.tsx
+++ src/app/components/Dashboard/index.tsx
@@ -1,27 +1,53 @@
-import React from 'react';
+"use client"
+
+import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 
+
 const VideoStatistics: React.FC = () => {
+    
+    const [videoFeedUrl, setVideoFeedUrl] = useState('http://traffic.aniccadatatest.com/video_feed');
+    const [vehicleCount, setVehicleCount] = useState<number>(0);
+
+    useEffect(() => {
+            const fetchVehicleCount = async () => {
+                try {
+                    const response = await fetch('http://traffic.aniccadatatest.com/vehicle_count');
+                    const data = await response.json();
+                    setVehicleCount(data.count);
+                } catch (error) {
+                    console.error('Error fetching vehicle count:', error);
+                }
+            };
+
+            fetchVehicleCount();
+            const intervalId = setInterval(fetchVehicleCount, 500);
+
+            return () => clearInterval(intervalId);
+
+    }, []);
+
     return (
         <>
             <div className="h-98px bg-white w-screen px-6 py-4 border-b-2">
                 <Navbar />
             </div>
-            <div className="grid grid-cols-3 gap-4 mt-10 mx-8 min-h-[600px]">
-                <div className="bg-white p-4 col-span-2 flex flex-col">
+            <div className="flex flex-row mt-10 mx-8 min-h-[600px] w-[98%] p-[2%]">
+                <div className="bg-white p-4 w-[65%] col-span-2 flex flex-col">
                     <div className='ml-6'>
                         Location: <span className='font-bold'>Anil Kumble Circle</span>
                     </div>
-                    <div className='ml-6 mt-10'>
-                        <iframe width="1000" height="500" src="https://www.youtube.com/embed/kJuqyyqxPV4?si=uni434GcH6AGqpSb&amp;controls=0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+                    <div className='ml-6 mt-10 w-[60vw] h-[60vh]'>
+                    {/* <Image alt='video_feed' width={200} height={100} src={"http://traffic.aniccadatatest.com/video_feed"} className='w-[60vw] h-[60vh]' title="Traffic Video Feed" /> */}
+                        <iframe width={800} height={500} src={videoFeedUrl} className='scale-[160%] ml-56 mt-32' title="Traffic Video Feed" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
                     </div>
                 </div>
-                <div className="bg-white p-4 col-span-1 flex flex-col gap-4">
-                    <div className='ml-6 font-bold text-lg border-b-2 py-2'>Metrics</div>
-                    <div className='mx-8 mt-4'>
+                <div className="bg-white w-[30%] col-span-1 flex flex-col items-center justify-top pt-10">
+                    <div className=' font-bold text-lg border-b-2 py-2'>Metrics</div>
+                    <div className='mt-4'>
                         <div className='w-[467px] h-[216px] bg-[#F2F8FF] p-6 flex flex-col'>
                             <div className='border-b-4 font-bold py-2'>Live Vehicle Count</div>
-                            <div className='mx-auto mt-8 text-5xl font-bold'>188</div>
+                            <div className='mx-auto mt-8 text-5xl font-bold'>{vehicleCount}</div>
                         </div>
                     </div>
                     <div className='ml-6 font-bold text-lg py-2 mt-4'>Vehicles Classification</div>
